cccc27eae7b89cf5e7c666709174596a05562f08
Castor <daniellenarduzzi@gmail.com>
2018-05-22 13:39:07 -0300

    funciones hasta deploy

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const Web3 = require('web3');
 var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8745"))
 const solc = require('solc');
+for (var i = 0; i < web3.eth.accounts.length; i++) {
+  acct[i] = web3.eth.accounts[i]
+}
+
 var contract = `pragma solidity ^0.4.15;
 
 contract NotarizeTx {
@@ -25,7 +29,7 @@ contract NotarizeTx {
   /*
     Contract constructor takes _user as client Ethereum address
    */
-  function NotarizeTx(address _buyer, address _seller, bytes32 _id, string _date, uint _value, bytes32 _hash, string _status, string _shipping) public {
+ constructor(address _buyer, address _seller, bytes32 _id, string _date, uint _value, bytes32 _hash, string _status, string _shipping) public {
     _tx.buyer = _buyer;
     _tx.seller = _seller;
     _tx.id = _id;
@@ -49,7 +53,7 @@ contract NotarizeTx {
       _tx.status = _status;
       _tx.hash = _hash;
       proofs[_hash] = _id;
-      NotaryEvt(_hash, _tx.id);
+    emit  NotaryEvt(_hash, _tx.id);
     } else {
       revert();
     }
@@ -65,11 +69,37 @@ contract NotarizeTx {
       _tx.status = _shipping;
       _tx.hash = _hash;
       proofs[_hash] = _tx.id;
-      NotaryEvt(_hash, _tx.id);
+      emit NotaryEvt(_hash, _tx.id);
     } else {
       revert();
     }
   }
 }`
-var compiled = solc.compile(contract)
-console.log(compiled);
+
+var compile = (contract) => solc.compile(contract)
+
+async function getData (contract) {
+  return {
+    abi: await JSON.parse(compile(contract).contracts[":NotarizeTx"].interface),
+    bytecode: await compile(contract).contracts[":NotarizeTx"].bytecode
+  }
+async function createContract(contract) {
+  return await web3.eth.contract(getData.abi(compile(contract)))
+}
+async function deploy(contract, buyer, seller, id, date, value, hash, status, shipping) {
+  return await createContract(getData.abi(compile(contract))).new( buyer, seller, id, date, value, hash, status, shipping, {
+    data: getData.bytecode,
+    gas: 3000000,
+  }, (err, contract) => {if(err)console.log(err);})
+}
+deploy(contract, acct[0], acct[1], "sksj3642ams6odnsoc32549102xfasf0", "2/4/18", 1, "sksj3642ams6odnsoc32549102xfasf1", "purchased", "da")
+  .then(
+    function (res) {
+      console.log(res);
+    },
+    function (err) {
+      console.log("Error: "+err)
+    }
+  )
+// var abi = JSON.parse(compiled.contracts.NotarizeTx.interface)
+// console.log(abi);
